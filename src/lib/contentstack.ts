75a3ae7aaddb7cb90d3945377b96e1cbe714743b
Tim Benniks <tbenniks@gmail.com>
2024-11-06 16:52:31 +0100

    Added modular blocks, version bumps, works outside of contentstack

--- src/lib/contentstack.ts
+++ src/lib/contentstack.ts
@@ -5,22 +5,26 @@ export async function getPage(live_preview: string, content_type_uid: string, en
   const deliveryToken = "cs620decb0e6bb175e31210ce9";
   const environment = "preview";
   const previewToken = "csa128deacffe0b26386090915";
-  const livePreviewHost = "eu-rest-preview.contentstack.com";
+  const region = "eu";
+
+  let hostname = region === "eu" ? "eu-cdn.contentstack.com" : "cdn.contentstack.com";
 
   const headers = new Headers();
   headers.append("Content-Type", "application/json");
   headers.append("api_key", apiKey);
   headers.append("access_token", deliveryToken);
 
-  headers.append("preview_token", previewToken);
-  headers.append("live_preview", live_preview);
+  if (live_preview !== '') {
+    headers.append("preview_token", previewToken);
+    headers.append("live_preview", live_preview);
+    hostname = region === "eu" ? "eu-rest-preview.contentstack.com" : "rest-preview.contentstack.com";;
+  }
 
-  const hostname = livePreviewHost;
   const url = `https://${hostname}/v3/content_types/${content_type_uid}/entries/${entry_uid}?environment=${environment}`
 
   const res = await fetch(url, {
     method: "GET",
-    headers: headers,
+    headers,
   });
 
   const result = await res.json();
